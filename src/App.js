6850ee8aa8b5aea867bb4392dd5a0abd3b2140b7
prayag2301 <prayagsharma2001@gmail.com>
2023-02-13 17:47:16 +0530

    Frontend changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import  { MyDropzoneComponent } from './components/FileUpload'
+import { MyDropzoneComponent } from './components/FileUpload'
 import './App.css';
 import ButtonAppBar from './components/ButtonAppBar';
 import { Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <ButtonAppBar />
-      <Typography align="center" variant="h2" color='primary' style={{
-        padding: '50px'
-      }}>
-        Content Based image retreival
-      </Typography>
-      <MyDropzoneComponent />
+    <div className='App'>
+      <div className="img">
+        <ButtonAppBar />
+        <Typography align="center" variant="h2" color='primary' style={{
+          padding: '50px'
+        }}>
+          Content Based Medical Image Retreival
+        </Typography>
+        <MyDropzoneComponent />
+      </div>
     </div>
   );
 }
--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -37,18 +37,18 @@ const options = { multi: true }
 
 // Render the file upload dropzone:
 export const MyDropzoneComponent = () =>
-<div className="box">
-  <UploadDropzone uploader={uploader}       // Required.
-                  options={options}         // Optional.
-                  width="600px"             // Optional.
-                  height="375px"            // Optional.
-                  onUpdate={files => {      // Optional.
-                    if (files.length === 0) {
-                      console.log('No files selected.')
-                    } else {
-                      console.log('Files uploaded:');
-                      console.log(files.map(f => f.fileUrl));
-                    }
-                  }} 
+  <div className="box">
+    <UploadDropzone uploader={uploader}       // Required.
+      options={options}         // Optional.
+      width="600px"             // Optional.
+      height="375px"            // Optional.
+      onUpdate={files => {      // Optional.
+        if (files.length === 0) {
+          console.log('No files selected.')
+        } else {
+          console.log('Files uploaded:');
+          console.log(files.map(f => JSON.stringify(f.fileUrl)));
+        }
+      }}
     />
-</div>
+  </div>
